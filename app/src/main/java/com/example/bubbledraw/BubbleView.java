4416f392b00ac64603cb13cc371e15ffa5c85fe2
Alexander Tih <dgioto@gmail.com>
2021-02-09 22:15:28 +0200

    Изменение класса Bubble

--- app/src/main/java/com/example/bubbledraw/BubbleView.java
+++ app/src/main/java/com/example/bubbledraw/BubbleView.java
@@ -1,6 +1,8 @@
 package com.example.bubbledraw;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
@@ -24,4 +26,35 @@ public class BubbleView extends ImageView implements View.OnTouchListener {
         bubbleList = new ArrayList<Bubble>();
     }
 
+    private class Bubble{
+        private int x;
+        private int y;
+        private int size;
+        private int color;
+        private int xspeed, yspeed;
+        private final int MAX_SPEED = 5;
+
+        public  Bubble(int newX, int newY, int newSize){
+            x = newX;
+            y = newY;
+            size = newSize;
+            color = Color.argb(rand.nextInt(256),
+                    rand.nextInt(256),
+                    rand.nextInt(256),
+                    rand.nextInt(256),);
+            xspeed = rand.nextInt(MAX_SPEED * 2 + 1) - MAX_SPEED;
+            yspeed = rand.nextInt(MAX_SPEED * 2 + 1) - MAX_SPEED;
+        }
+
+        public void draw(Canvas canvas){
+            myPaint.setColor(color);
+            canvas.drawOval(x - size/2, y - size/2,
+                    x + size/2, y + size/2, myPaint);
+        }
+
+        public void update(){
+            x += xspeed;
+            y += yspeed;
+        }
+    }
 }
