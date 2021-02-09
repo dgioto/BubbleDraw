82304ffb65c38a7077dfe1b8a70f4b53b7def7c1
Alexander Tih <dgioto@gmail.com>
2021-02-09 13:40:47 +0200

    Создаем проект "Рисование пузырьков"

---
+++ app/src/main/java/com/example/bubbledraw/BubbleView.java
@@ -0,0 +1,9 @@
+package com.example.bubbledraw;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.ImageView;
+
+public class BubbleView extends ImageView implements View.OnTouchListener {
+
+}
